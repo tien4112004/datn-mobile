d9f89258bfc7664b4cfb1fe4cc58fcf0d9c06655
ltt204 <trongtin.cv@gmail.com>
2026-01-04 13:51:58 +0700

    feat: mindmap detail wip

---
+++ lib/features/projects/ui/pages/mindmap/mindmap_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:datn_mobile/features/projects/states/controller_provider.dart';
+import 'package:datn_mobile/features/projects/ui/widgets/mindmap/mindmap_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+@RoutePage()
+class MindmapDetailPage extends ConsumerWidget {
+  final String mindmapId;
+  const MindmapDetailPage({
+    super.key,
+    @PathParam('mindmapId') required this.mindmapId,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MindmapDetail(
+      mindmapId: mindmapId,
+      onClose: () => context.router.maybePop(),
+      onRetry: () => ref.invalidate(mindmapByIdProvider(mindmapId)),
+    );
+  }
+}

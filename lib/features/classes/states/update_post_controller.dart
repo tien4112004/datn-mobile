4ac3a1e6ff735272376049b91c9688f27ad8aa75
ltt204 <trongtin.cv@gmail.com>
2026-01-08 18:02:58 +0700

    feat: enhance posts list

--- lib/features/classes/states/update_post_controller.dart
+++ lib/features/classes/states/update_post_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:datn_mobile/features/classes/domain/entity/post_type.dart';
+import 'package:datn_mobile/features/classes/providers/post_paging_controller_pod.dart';
 import 'package:datn_mobile/features/classes/states/posts_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -39,7 +40,7 @@ class UpdatePostController extends AsyncNotifier<void> {
       );
 
       // Refresh the posts list
-      ref.invalidate(postsControllerProvider(classId));
+      ref.invalidate(postPagingControllerPod(classId));
     });
   }
 
@@ -55,8 +56,8 @@ class UpdatePostController extends AsyncNotifier<void> {
       final repository = ref.read(postRepositoryProvider);
       await repository.togglePin(postId, pinned);
 
-      // Refresh the posts list
-      ref.invalidate(postsControllerProvider(classId));
+      final pagingController = ref.read(postPagingControllerPod(classId));
+      pagingController.refresh();
     });
   }
 }

70cddff4406a5bc157667ea0296677b6b2b16e5f
ltt204 <trongtin.cv@gmail.com>
2026-01-09 22:01:08 +0700

    chore: small fix

--- lib/features/home/main_wrapper_page.dart
+++ lib/features/home/main_wrapper_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:datn_mobile/core/router/router.gr.dart';
 import 'package:datn_mobile/features/auth/controllers/user_controller.dart';
+import 'package:datn_mobile/features/auth/domain/entities/user_role.dart';
+import 'package:datn_mobile/shared/riverpod_ext/async_value_easy_when.dart';
 import 'package:datn_mobile/shared/widgets/no_internet_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -59,137 +61,136 @@ class MainWrapperPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userState = ref.watch(userControllerProvider);
-    final userStateNotifier = ref.watch(userControllerProvider.notifier);
-    final isStudent = userStateNotifier
-        .isStudent(); // If role has value, the user is student
+    return userState.easyWhen(
+      data: (userProfileState) {
+        final isStudent = userState.value?.role == UserRole.student;
+        final routes = isStudent
+            ? [const ClassRoute(), const SettingRoute()]
+            : [
+                const HomeRoute(),
+                const ProjectsRoute(),
+                const ClassRoute(),
+                const SettingRoute(),
+              ];
 
-    final routes = isStudent
-        ? [const ClassRoute(), const SettingRoute()]
-        : [
-            const HomeRoute(),
-            const ProjectsRoute(),
-            const ClassRoute(),
-            const SettingRoute(),
-          ];
-    debugPrint('User is student: $isStudent');
-    debugPrint('User role: ${userState.value?.role}');
+        return AutoTabsScaffold(
+          routes: routes,
+          bottomNavigationBuilder: (_, tabsRouter) {
+            if (userState.isLoading) {
+              return const SizedBox.shrink();
+            }
 
-    return AutoTabsScaffold(
-      routes: routes,
-      bottomNavigationBuilder: (_, tabsRouter) {
-        if (userState.isLoading) {
-          return const SizedBox.shrink();
-        }
-
-        return Theme(
-          data: Theme.of(context).copyWith(
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-          ),
-          child: BottomNavigationBar(
-            currentIndex: tabsRouter.activeIndex,
-            onTap: tabsRouter.setActiveIndex,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            type: BottomNavigationBarType.shifting,
-            items: isStudent
-                ? [
-                    BottomNavigationBarItem(
-                      icon: _bottomItemActivated(
-                        LucideIcons.school,
-                        "Class",
-                        false,
-                        context,
-                      ),
-                      activeIcon: _bottomItemActivated(
-                        LucideIcons.school,
-                        "Class",
-                        true,
-                        context,
-                      ),
-                      label: "",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: _bottomItemActivated(
-                        LucideIcons.user,
-                        "Profile",
-                        false,
-                        context,
-                      ),
-                      activeIcon: _bottomItemActivated(
-                        LucideIcons.user,
-                        "Profile",
-                        true,
-                        context,
-                      ),
-                      label: "",
-                    ),
-                  ]
-                : [
-                    BottomNavigationBarItem(
-                      icon: _bottomItemActivated(
-                        LucideIcons.house,
-                        "Home",
-                        false,
-                        context,
-                      ),
-                      activeIcon: _bottomItemActivated(
-                        LucideIcons.house400,
-                        "Home",
-                        true,
-                        context,
-                      ),
-                      label: "",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: _bottomItemActivated(
-                        LucideIcons.folder,
-                        "Project",
-                        false,
-                        context,
-                      ),
-                      activeIcon: _bottomItemActivated(
-                        LucideIcons.folder,
-                        "Project",
-                        true,
-                        context,
-                      ),
-                      label: "",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: _bottomItemActivated(
-                        LucideIcons.school,
-                        "Class",
-                        false,
-                        context,
-                      ),
-                      activeIcon: _bottomItemActivated(
-                        LucideIcons.school,
-                        "Class",
-                        true,
-                        context,
-                      ),
-                      label: "",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: _bottomItemActivated(
-                        LucideIcons.user,
-                        "Profile",
-                        false,
-                        context,
-                      ),
-                      activeIcon: _bottomItemActivated(
-                        LucideIcons.user,
-                        "Profile",
-                        true,
-                        context,
-                      ),
-                      label: "",
-                    ),
-                  ],
-          ),
-        );
+            return Theme(
+              data: Theme.of(context).copyWith(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+              ),
+              child: BottomNavigationBar(
+                currentIndex: tabsRouter.activeIndex,
+                onTap: tabsRouter.setActiveIndex,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                type: BottomNavigationBarType.shifting,
+                items: isStudent
+                    ? [
+                        BottomNavigationBarItem(
+                          icon: _bottomItemActivated(
+                            LucideIcons.school,
+                            "Class",
+                            false,
+                            context,
+                          ),
+                          activeIcon: _bottomItemActivated(
+                            LucideIcons.school,
+                            "Class",
+                            true,
+                            context,
+                          ),
+                          label: "",
+                        ),
+                        BottomNavigationBarItem(
+                          icon: _bottomItemActivated(
+                            LucideIcons.user,
+                            "Profile",
+                            false,
+                            context,
+                          ),
+                          activeIcon: _bottomItemActivated(
+                            LucideIcons.user,
+                            "Profile",
+                            true,
+                            context,
+                          ),
+                          label: "",
+                        ),
+                      ]
+                    : [
+                        BottomNavigationBarItem(
+                          icon: _bottomItemActivated(
+                            LucideIcons.house,
+                            "Home",
+                            false,
+                            context,
+                          ),
+                          activeIcon: _bottomItemActivated(
+                            LucideIcons.house400,
+                            "Home",
+                            true,
+                            context,
+                          ),
+                          label: "",
+                        ),
+                        BottomNavigationBarItem(
+                          icon: _bottomItemActivated(
+                            LucideIcons.folder,
+                            "Project",
+                            false,
+                            context,
+                          ),
+                          activeIcon: _bottomItemActivated(
+                            LucideIcons.folder,
+                            "Project",
+                            true,
+                            context,
+                          ),
+                          label: "",
+                        ),
+                        BottomNavigationBarItem(
+                          icon: _bottomItemActivated(
+                            LucideIcons.school,
+                            "Class",
+                            false,
+                            context,
+                          ),
+                          activeIcon: _bottomItemActivated(
+                            LucideIcons.school,
+                            "Class",
+                            true,
+                            context,
+                          ),
+                          label: "",
+                        ),
+                        BottomNavigationBarItem(
+                          icon: _bottomItemActivated(
+                            LucideIcons.user,
+                            "Profile",
+                            false,
+                            context,
+                          ),
+                          activeIcon: _bottomItemActivated(
+                            LucideIcons.user,
+                            "Profile",
+                            true,
+                            context,
+                          ),
+                          label: "",
+                        ),
+                      ],
+              ),
+            );
+          },
+        ).monitorConnection();
       },
-    ).monitorConnection();
+    );
   }
 }
